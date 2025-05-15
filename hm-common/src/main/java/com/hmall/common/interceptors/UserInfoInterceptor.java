8b46a7e5889153a27c97ffb240fa1399888933d8
Diamond <2210054954@qq.com>
2025-05-15 15:20:54 +0800

    网关传递用户信息到微服务

---
+++ hm-common/src/main/java/com/hmall/common/interceptors/UserInfoInterceptor.java
@@ -0,0 +1,32 @@
+package com.hmall.common.interceptors;
+
+import cn.hutool.core.util.StrUtil;
+import com.hmall.common.utils.UserContext;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Diamond
+ * @create 2025-05-15 15:06
+ */
+public class UserInfoInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1.获取用户信息
+        String header = request.getHeader("user-info");
+        // 2.判断是否获取了用户，如果有，则存入ThreadLocal
+        if (StrUtil.isNotBlank(header)) {
+            UserContext.setUser(Long.valueOf(header));
+        }
+        // 3.放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // 清理用户
+        UserContext.removeUser();
+    }
+}
