ed0c7e728009c1cf73413002603fa6dd0965c5f1
Diamond <2210054954@qq.com>
2025-05-15 14:17:25 +0800

    网关的使用

---
+++ hm-gateway/src/main/java/com/hmall/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.hmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Diamond
+ * @create 2025-05-15 13:17
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
